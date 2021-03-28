39119f5ab43e3521a13360a9e17dddaf0d8a811c
DananjayaBC <Dana.chanuka@gmail.com>
2021-03-28 12:45:57 +0530

    SihnUp and signIn routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails'
+import signin from './components/auth/SignIn'
+import signUp from './components/auth/SignUp'
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <Switch>
           <Route exact path='/' component={Dashboard} />
           <Route path='/project/:id' component={ProjectDetails} />
+          <Route path='/signin' component={signin} />
+          <Route path='/signUp' component={signUp}/>
         </Switch>
       </div>
     </BrowserRouter>
