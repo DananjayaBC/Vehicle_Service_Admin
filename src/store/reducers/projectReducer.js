23888c6ba806334af8709b0f65eff5be29907566
DananjayaBC <Dana.chanuka@gmail.com>
2021-03-28 22:32:18 +0530

    Create Reducers

---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,12 @@
+const initState = {
+    projects:[
+        {id: '1', title:'help me fined peach', content:'blah blah blah'},
+        {id: '2', title:'collect all the stars', content:'blah blah blah'},
+        {id: '3', title:'egg hunt with dana', content:'blah blah blah'},       
+    ]
+}
+
+const projectReducer = (state = initState, action) => {
+    return state
+}
+export default projectReducer
