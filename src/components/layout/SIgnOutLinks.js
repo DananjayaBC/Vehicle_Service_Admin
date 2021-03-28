9f152e9de08a8b4ea4dd66fa1ddcb060fd5c7aad
DananjayaBC <Dana.chanuka@gmail.com>
2021-03-28 22:31:46 +0530

    Add Links

--- src/components/layout/SIgnOutLinks.js
+++ src/components/layout/SIgnOutLinks.js
@@ -3,8 +3,8 @@ import { NavLink } from 'react-router-dom'
 const SingedOutLinks = () => {
     return (
         <ul className="right">
-            <li><NavLink to="/">Signup</NavLink></li>
-            <li><NavLink to="/">Login</NavLink></li>
+            <li><NavLink to="/signup">Signup</NavLink></li>
+            <li><NavLink to="/signin">Login</NavLink></li>
         </ul>
     )
 }
--- src/components/layout/SigndinLinks.js
+++ src/components/layout/SigndinLinks.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 const SingedInLinks = () =>{
     return(
        <ul className="right">
-           <li><NavLink to="/">New Project</NavLink></li>
+           <li><NavLink to="/create">New Project</NavLink></li>
            <li><NavLink to="/">Log Out</NavLink></li>
            <li><NavLink to="/" className='btn btn-floating pink lighten-1'>VA</NavLink></li>
        </ul>
