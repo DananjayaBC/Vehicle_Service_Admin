29c0878fc0d2958befaddaa9a9f905684b823344
DananjayaBC <Dana.chanuka@gmail.com>
2021-03-30 17:43:02 +0530

    User profileData

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,9 +5,9 @@ import { connect } from 'react-redux'
 
 
 const Navbar = (props) => {
-    const { auth } = props;
+    const { auth,profile } = props;
     //console.log(auth)
-    const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
+    const links = auth.uid ? <SignedInLinks profile={profile}/> : <SignedOutLinks />
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
@@ -21,7 +21,8 @@ const Navbar = (props) => {
 const mapStateToProps = (state) => {
     console.log(state);
     return {
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        profile: state.firebase.profile
     }
 }
 
--- src/components/layout/SigndinLinks.js
+++ src/components/layout/SigndinLinks.js
@@ -7,7 +7,7 @@ const SingedInLinks = (props) => {
         <ul className="right">
             <li><NavLink to="/create">New Project</NavLink></li>
             <li><a onClick={props.signOut}> Log Out</a></li>
-            <li><NavLink to="/" className='btn btn-floating pink lighten-1'>VA</NavLink></li>
+            <li><NavLink to="/" className='btn btn-floating pink lighten-1'>{props.profile.initials}</NavLink></li>
         </ul>
     )
 }
