58537260d2dc1e4e1476fb459e64cfb5594e9553
DananjayaBC <Dana.chanuka@gmail.com>
2021-03-30 11:21:54 +0530

    Route Guarding

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 class SignUp extends Component {
     state = {
@@ -17,6 +19,9 @@ class SignUp extends Component {
         console.log(this.state);
     }
     render() {
+        const { auth } = this.props;
+        if (auth.uid) return <Redirect to='/' />
+
         return (
             <div className="container">
                 <form onSubmit={this.handleSubmit} className="white">
@@ -45,5 +50,10 @@ class SignUp extends Component {
         )
     }
 }
+const mapStateToProps = (state) => {
+    return {
+        auth: state.firebase.auth
+    }
+}
 
-export default SignUp
+export default connect(mapStateToProps)(SignUp)
