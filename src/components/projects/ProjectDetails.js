acb9f97a76cf63b2137e1617c2a876a6d457a45c
DananjayaBC <Dana.chanuka@gmail.com>
2021-03-31 11:06:14 +0530

    Formating dates

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
 import { Redirect } from 'react-router-dom'
+import moment from 'moment'
 
 const ProjectDetails = (props) => {
     const { project, auth } = props;
@@ -18,7 +19,7 @@ const ProjectDetails = (props) => {
                     </div>
                     <div className="card-action gret lghten-4 grye-text">
                         <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
-                        <div>28th March</div>
+                        <div>{moment(project.createdAt.toDate()).calendar()}</div>
                     </div>
                 </div>
             </div>
